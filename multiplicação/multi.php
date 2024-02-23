a1228301402b0892a473262a241af5ac006e7e95
Arthur M <143713077+murraythur@users.noreply.github.com>
2024-02-23 13:30:33 -0300

    multi.php

---
+++ multiplicação/multi.php
@@ -0,0 +1,16 @@
+<?php 
+
+echo "-inserta 2 numero e seu produto será exibido.\n";
+
+$num_1 = readline("1°: ");
+$num_2 = readline("2°: ");
+
+$resultado = 0;
+
+for ($i = 0; $i < $num_2; $i++) {
+    
+    $resultado += $num_1;
+
+}
+
+echo "produtto". $resultado . "\n";
